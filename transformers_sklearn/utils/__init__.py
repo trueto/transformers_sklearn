a12b7427caca19d16df2bcfcf17d6f4416235771
trueto <18868150826@163.com>
2020-04-11 17:17:35 +0800

    add model for el task

--- transformers_sklearn/utils/__init__.py
+++ transformers_sklearn/utils/__init__.py
@@ -1,5 +1,5 @@
 from .classification_utils import ClassificationProcessor,\
     load_and_cache_examples,acc_and_f1
 
-from .features_utils import BertForSequenceVector
+from .features_utils import BertForSequenceVector, BertForTokenVector
 from .loss_utils import FocalLoss
